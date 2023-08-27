48f5d204ba9264b22a7dcf0bb578ad503a37fbff
feer921 <feer921@163.com>
2023-08-27 20:28:50 +0800

    feat:升级依赖库版本;feat: 完善控件功能

---
+++ app/src/main/java/com/fee/formatshowtextview/FormatShowTextDemoActivity.kt
@@ -0,0 +1,13 @@
+package com.fee.formatshowtextview
+
+/**
+ ******************(^_^)***********************<br>
+ * Author: fee(QQ/WeiXin:1176610771)<br>
+ * Date: 2023/8/26<br>
+ * Time: 09:29<br>
+ * <P>DESC:
+ * 
+ * </p>
+ * ******************(^_^)***********************
+ */class FormatShowTextDemoActivity {
+}
